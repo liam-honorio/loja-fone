e314e99ed6af48fb08c48d05fdee740a6c55a31e
William Honorio <mhf.eng.dsg@gmail.com>
2019-08-16 12:27:10 -0300

    Aula 15 - Desafio. Falta Debugar estrelas

--- js/description-product.js
+++ js/description-product.js
@@ -1,5 +1,50 @@
 const $heart = document.querySelector(".-heart");
+const $firstStar = document.querySelectorAll(".star");
+let teste = [];
 
-$heart.addEventListener("click", () => {
-  $heart.classList.toggle("-active");
+$firstStar.forEach(function(item) {
+  teste.push(item);
+});
+
+console.log(teste);
+
+$heart.addEventListener("click", handleClick);
+
+// $firstStar.forEach(function($star) {
+//   $star.addEventListener("click", handleClick);
+// });
+
+// function handleClick() {
+//   this.classList.toggle("-active");
+// }
+
+// $heart.addEventListener("click", () => {
+//   $heart.classList.toggle("-active");
+// });
+
+// $firstStar.forEach($star => {
+//   $star.addEventListener("click", () => {
+//     $star.classList.toggle("-active");
+//   });
+// });
+
+// $firstStar.forEach(function($star) {
+//   $star.addEventListener("click", handleClick);
+// });
+
+// function handleClick() {
+//   this.classList.toggle("-active");
+// }
+
+teste.forEach($star => {
+  $star.addEventListener("click", () => {
+    $star.classList.toggle("-active");
+    index = teste.indexOf($star);
+    i = 0;
+    total = 4;
+    while (i < index) {
+      teste[i].classList.toggle("-active");
+      i++;
+    }
+  });
 });
