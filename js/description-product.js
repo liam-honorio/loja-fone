1207a91cba5b7f6e19f9c7f65a73149ffe4ec157
William Honorio <mhf.eng.dsg@gmail.com>
2019-08-19 11:55:51 -0300

    Algoritmo limpo e corrigido das estrelas

--- js/description-product.js
+++ js/description-product.js
@@ -1,78 +1,45 @@
 const $heart = document.querySelector(".-heart");
-const $firstStar = document.querySelectorAll(".star");
-let teste = [];
-
-$firstStar.forEach(function(item) {
-  teste.push(item);
-});
-
-console.log(teste);
+const $stars = document.querySelectorAll(".star");
+const positionLast = $stars.length - 1;
 
 $heart.addEventListener("click", handleClick);
 
-// $firstStar.forEach(function($star) {
-//   $star.addEventListener("click", handleClick);
-// });
-
-// function handleClick() {
-//   this.classList.toggle("-active");
-// }
-
-// $heart.addEventListener("click", () => {
-//   $heart.classList.toggle("-active");
-// });
-
-// $firstStar.forEach($star => {
-//   $star.addEventListener("click", () => {
-//     $star.classList.toggle("-active");
-//   });
-// });
-
-// $firstStar.forEach(function($star) {
-//   $star.addEventListener("click", handleClick);
-// });
-
-// function handleClick() {
-//   this.classList.toggle("-active");
-// }
-
-teste.forEach($star => {
-  $star.addEventListener("click", () => {
-    //adiciona estrela//
-    index = teste.indexOf($star);
-    i = 0;
-    if (!$star.classList.contains("-active")) {
-      $star.classList.add("-active");
-      while (i < index) {
-        teste[i].classList.add("-active");
-        i++;
-      }
-    } else {
-      $star.classList.remove("-active");
-      i = index;
-      while (i <= 4) {
-        teste[i].classList.remove("-active");
-        i++;
-      }
-    }
-  });
+$stars.forEach(function($star, key) {
+  if (key == 0) {
+    $star.addEventListener("click", firstStar);
+  }
+  if (key == positionLast) {
+    $star.addEventListener("click", lastStar);
+  }
+  if (key > 0 && key < positionLast) {
+    $star.addEventListener("click", function() {
+      middleStar(key);
+    });
+  }
 });
 
-// $firstStar.forEach((star, index) => {
-//   star.addEventListener("click", () => {
-//     handleStar(index);
-//   });
-// });
+function handleClick() {
+  this.classList.toggle("-active");
+}
 
-// function handleStar(index) {
-//   const tamanho = $firstStar.length - 1;
-//   if ($firstStar[index].classList.contains("-active")) {
-//     for (let i = tamanho; i > index; i--) {
-//       $firstStar[i].classList.remove("-active");
-//     }
-//   } else {
-//     for (let i = 0; i <= index; i++) {
-//       $firstStar[i].classList.add("-active");
-//     }
-//   }
-// }
+function firstStar() {
+  $stars.forEach(function($star) {
+    $star.classList.remove("-active");
+  });
+  this.classList.add("-active");
+}
+
+function middleStar(index) {
+  $stars.forEach(function($star, key) {
+    $star.classList.remove("-active");
+    if (key <= index) {
+      $star.classList.add("-active");
+    }
+  });
+}
+
+function lastStar() {
+  $stars.forEach(function($star) {
+    $star.classList.add("-active");
+  });
+}
